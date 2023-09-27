ae6feb242b1349207b74c4709322d377f42ce745
Tamakuz <jovangithub@gmail.com>
2023-09-27 12:11:23 +0700

    fix mongo db atlas

--- index.js
+++ index.js
@@ -7,25 +7,22 @@ import router from "./src/routes/router.js";
 const app = express();
 const port = process.env.PORT || 5000;
 
-// YqzBKvNdz14GhIpP
-// mongoose.connect("mongodb://127.0.0.1:27017/personal_branding", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+
+// const url = "mongodb://127.0.0.1:27017/personal_branding";
 const url =
   "mongodb+srv://jovangithub:YqzBKvNdz14GhIpP@personal.cl2jypz.mongodb.net/personal_branding?retryWrites=true&w=majority";
+
 mongoose
   .connect(url)
   .then(() => {
     console.log("Successfully connected to the database");
   })
   .catch((err) => {
-    console.log("Could not connect to the database. Exiting now...", err);
-    process.exit();
+    console.log(err);
   });
 
 app.get("/", (req, res) => {
