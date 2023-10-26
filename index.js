ca81ac0be9c5e683ccadaf73a64663ff3afbda37
Tamakuz <jovangithub@gmail.com>
2023-10-26 13:17:02 +0700

    ADD: custom origin cors

--- index.js
+++ index.js
@@ -7,20 +7,18 @@ import router from "./src/routes/router.js";
 const app = express();
 const port = process.env.PORT || 5000;
 
-const allowedOrigins = ["http://localhost:5173"];
-
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: allowedOrigins
+    origin: ["http://localhost:5173"],
   })
 );
 
 
 // const url = "mongodb://127.0.0.1:27017/personal_branding";
 const url =
-  "mongodb+srv://jovangithub:YqzBKvNdz14GhIpP@personal.cl2jypz.mongodb.net/personal_branding?retryWrites=true&w=majority";
+  "mongodb+srv://jovangithub:jovangithub>@personal.cl2jypz.mongodb.net/personal_branding?retryWrites=true&w=majority";
 
 mongoose
   .connect(url)
